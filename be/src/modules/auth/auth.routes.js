fe2e5ab35b07cbb1e632e6a743432c2289833497
nishchay-011bq <nischay@bytequests.com>
2026-07-22 18:23:38 +0530

    refactor: remove OTP table migration and update reset password route to include sanitized input

--- be/src/modules/auth/auth.routes.js
+++ be/src/modules/auth/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { fetchMeController, forgotPasswordController, loginController, RegisterController, resetPasswordController, verifyOtpController } from "./auth.controller.js";
-import { sanitizedLoginInput, sanitizedRegisterInput, sanitizedVerifyOtpInput } from "./auth.sanitizedInput.js";
+import { sanitizedLoginInput, sanitizedRegisterInput, sanitizedResetPasswordInput, sanitizedVerifyOtpInput } from "./auth.sanitizedInput.js";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -10,6 +10,6 @@ router.post("/verify-otp", sanitizedVerifyOtpInput, verifyOtpController);
 router.post("/login", sanitizedLoginInput, loginController);
 router.get("/me", authMiddleware, fetchMeController)
 router.post("/forgot-password", forgotPasswordController)
-router.post("/reset-password", resetPasswordController)
+router.post("/reset-password", sanitizedResetPasswordInput, resetPasswordController)
 
 export default router;
